fa34568850fb8129f9455a271e7b71a2b896ed24
hongmaogui <2415070262@qq.com>
2020-04-14 19:21:45 +0800

    add list

---
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -0,0 +1,36 @@
+package life.majiang.community.community.dto;
+
+/**
+ * @author ly
+ * @Description TODO
+ * @date 2020/4/14
+ */
+public class GithubUser {
+    private String name;
+    private long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
